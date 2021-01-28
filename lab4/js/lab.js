5e476c533f6886fd0e176faaaf4907d1d85d11db
Naomi R <naomimiriam1999@gmail.com>
2021-01-28 01:39:52 -0800

    uploaded lab 4 and 5

---
+++ lab4/js/lab.js
@@ -0,0 +1,9 @@
+//Create a border of limitations
+//Establish point of interaction for player
+  //Create paddle to hit the ball with
+//Generate a ball for interaction
+  //Establish ball’s possible trajectories
+    //Randomly generate inputs for ball’s trajectory
+//Establish point system for when the player scores
+//Create a target for scoring points
+//Accept player input
