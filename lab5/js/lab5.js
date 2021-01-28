b8b010283e43ded3fea622b2888c1c2460bb7b32
naomirumph <77122071+naomirumph@users.noreply.github.com>
2021-01-28 01:37:41 -0800

    Add files via upload

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Naomi Rumph <nrumph@ucsc.edu>
+ * Created: 28 January
+ * License: Public Domain
+ */
+
+ // Define Variables
+ make = "Toyota";
+ model = "Prius";
+ color = "Red";
+ year = 2005;
+
+ // calculate
+ age = 2021 - year;
+
+ //output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
