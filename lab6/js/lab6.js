47879263377c5442d1bea495c118daff55e52d38
Naomi R <naomimiriam1999@gmail.com>
2021-02-01 22:15:54 -0800

    updated lab 6

---
+++ lab6/js/lab6.js
@@ -0,0 +1,25 @@
+/*
+ * Author:  Naomi Rumph <nrumph@ucsc.edu>
+ * Created:  1 February
+ * License:  Public Domain
+ */
+
+// Define Variables
+
+ mytransport = ["foot", "car", "bus"];
+
+ myMainRide = {
+    make = "Toyota";
+    model = "Prius";
+    color = "red";
+    year = 2005;
+    age : function() {
+        return 2019 - age:
+    }
+}
+
+
+ // output
+document.writeln("Kinds of transportation I use:", myTransport, "</br>")
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
